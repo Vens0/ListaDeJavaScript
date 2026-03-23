73e17a9f3650df571acffed38efc75ed41a1129d
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-03-23 12:38:14 -0300

    add - Exemplo 3 terminado

--- scripts/Ex03.js
+++ scripts/Ex03.js
@@ -1,18 +1,19 @@
 
 function salario() {
 
-    let gastot;
+    let gastot = 0;
     let gasto = 0;
     let soma = 0;
     let ganho = 0;
+    let saldo = 0;
 
     for (let i = 1; i <= 12; i++) {
 
-        let ganho = parseFloat(prompt("Informe o seus ganhos no mês" + i));
+        let ganho = parseFloat(prompt("Informe o seus ganhos no mês " + i));
         if (!isNaN(ganho)) {
             soma += ganho;
 
-            let gasto = parseFloat(prompt("Informe os seus gastos do mês"));
+            let gasto = parseFloat(prompt("Informe os seus gastos do mês " + i));
             gastot += gasto;
 
 
@@ -20,10 +21,18 @@ function salario() {
             alert("Digite um número válido.");
         }
 
-
-
         console.log(soma);
         console.log(gastot);
 
     }
+
+    saldo = soma - gastot;
+    prompt("O seu saldo ao final do ano é de " + saldo);
+
+    if (saldo < 0){
+        prompt("Você ficou no prejuízo!");
+    }else{
+        prompt("Você obteve lucro!");
+    }
+
 }

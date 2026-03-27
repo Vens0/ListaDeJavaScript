8bdd86aed900962ca8e63b6a3be6031191b4c175
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-03-27 12:02:11 -0300

    up - Arrumei 10

--- scripts/Ex10.js
+++ scripts/Ex10.js
@@ -4,8 +4,10 @@ function sal(){
     let sala = parseFloat(prompt("Informe o seu salário:"));
     alert("Tenha em mente que será descontado 8% devido a taxa do INSS.");
 
-    let salad = sala - (sala*8)/100;
 
-    alert("Informações do usuário:\n" + "Nome: " + nome + "\nSalário bruto: " + sala + "\nSalário líquido: " + salad);
+    let desc = 0.08;
+    let salad = sala - sala*desc;
+
+    alert("Informações do usuário:\n" + "Nome: " + nome + "\nSalário bruto: " + sala + "\nValor do desconto: 8%" + "\nSalário líquido: " + salad);
 
 }

614e110ba6d03c07e9ca417ba4159e5435d7f08a
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-03-27 12:26:21 -0300

    add - Exemplo 11 quase terminado

--- scripts/Ex10.js
+++ scripts/Ex10.js
@@ -5,9 +5,9 @@ function sal(){
     alert("Tenha em mente que será descontado 8% devido a taxa do INSS.");
 
 
-    let desc = 0.08;
-    let salad = sala - sala*desc;
+    let desc = sala*0.08;
+    let salad = sala - desc;
 
-    alert("Informações do usuário:\n" + "Nome: " + nome + "\nSalário bruto: " + sala + "\nValor do desconto: 8%" + "\nSalário líquido: " + salad);
+    alert("Informações do usuário:\n" + "Nome: " + nome + "\nSalário bruto: " + sala + "\nValor do desconto: " + desc + "\nSalário líquido: " + salad);
 
 }
---
+++ scripts/Ex11.js
@@ -0,0 +1,30 @@
+function faixa() {
+
+    let sal = prompt("Informe a sua faixa salarial:");
+
+    if (sal <= 1000) {
+
+        let taxa = 8;
+        let desc = sal - (sal * taxa) / 100;
+        let liq = sal - desc;
+
+        alert("Informações do usuário:" + "\nSalário bruto: " + sal + "\nTaxa de desconto: " + taxa + "\nValor do desconto: " + liq + "\nSalário líquido: " + desc);
+
+    } else if (sal > 1000 || sal < 1500) {
+        let taxa = 8.5;
+        let desc = sal - (sal * taxa) / 100;
+        let liq = sal - desc;
+
+        alert("Informações do usuário:" + "\nSalário bruto: " + sal + "\nTaxa de desconto: " + taxa + "\nValor do desconto: " + liq + "\nSalário líquido: " + desc);
+
+    } else if (sal > 1500) {
+
+        let taxa = 9;
+        let desc = sal - (sal * taxa) / 100;
+        let liq = sal - desc;
+
+        alert("Informações do usuário:" + "\nSalário bruto: " + sal + "\nTaxa de desconto: " + taxa + "\nValor do desconto: " + liq + "\nSalário líquido: " + desc);
+
+    }
+
+}
